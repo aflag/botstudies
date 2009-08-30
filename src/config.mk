7ac3cd77a1ae453e05c96c51795e5be91acd81bd
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2009-08-30 12:26:53 -0300

    Creation of a config file for the Makefile

---
+++ src/config.mk
@@ -0,0 +1,16 @@
+# pde filename
+target=mouse
+
+# External programs
+CC=avr-gcc
+CXX=avr-g++
+AR=avr-ar
+OBJCOPY=avr-objcopy
+AVRDUDE=avrdude -F -V
+RM=rm -f
+
+# Upload configuration
+PORT=/dev/ttyUSB0
+BAUD=57600
+PROTOCOL=stk500v1
+PART=m328p
